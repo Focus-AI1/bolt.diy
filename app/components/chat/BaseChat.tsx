35de457fbbbae7cd01d3dd0493d90450d130fabf
willatfocusai <will@focus-ai.com>
2025-04-28 16:58:20 -0400

    Staging to Main for PostHog (#829)
    * commit separate railway dockerfile... we will have to change this soon, but hopefully it will work for now
    
    * update railway.toml for bolt.diy
    
    * welp
    
    * maybe this time??
    
    * if after the 12th time you dont succeed, try, try, try, try... more...
    
    * if after the 13th time you dont succeed, try, try, try, try... more...
    
    * if after the 14th time you dont succeed, try, try, try, try... more...
    
    * ahh (#810)
    
    * added headers for cloudflare (#811)
    
    * trying another way (#812)
    
    * another attempt (#813)
    
    * adding headers to public (#814)
    
    * focus copilot landing page
    
    * copilot landing sign up page
    
    * one more (#816)
    
    * copilot user flow
    
    * Improvements to the dashboard and org view (#818)
    
    * bolt on landing page for copilottt (#820)
    
    * ui changes
    
    * focus branding
    
    * full viewport
    
    * ask focus (#825)
    
    * Spacing for Copilot Landing Page (#826)
    
    * ask focus
    
    * spacing for base chat
    
    * Posthog copilot prism (#827)
    
    * ask focus
    
    * spacing for base chat
    
    * copilot user tracking
    
    * Posthog copilot prism (#828)
    
    * ask focus
    
    * spacing for base chat
    
    * copilot user tracking
    
    * posthog updates
    
    ---------
    
    Co-authored-by: keegan poppen <keegan@focus-ai.com>
    Co-authored-by: Jason Jin <jason@focus-ai.com>
    Co-authored-by: joey-sabarese <joe@focus-ai.com>
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- app/components/chat/BaseChat.tsx
+++ app/components/chat/BaseChat.tsx
@@ -324,14 +324,14 @@ export const BaseChat = React.forwardRef<HTMLDivElement, BaseChatProps>(
         data-chat-visible={showChat}
       >
         <ClientOnly>{() => <Menu />}</ClientOnly>
-        <div ref={scrollRef} className="flex flex-col lg:flex-row overflow-y-auto w-full h-full">
+        <div ref={scrollRef} className="flex flex-col lg:flex-row w-full h-full">
           <div className={classNames(styles.Chat, 'flex flex-col flex-grow lg:min-w-[var(--chat-min-width)] h-full')}>
             {!chatStarted && (
-              <div id="intro" className="mt-[16vh] max-w-chat mx-auto text-center px-4 lg:px-0">
+              <div id="intro" className="mt-[5vh] max-w-chat mx-auto text-center px-4 lg:px-0">
                 <h1 className="text-3xl lg:text-6xl font-bold text-bolt-elements-textPrimary mb-4 animate-fade-in">
                   Where tickets become prototypes
                 </h1>
-                <p className="text-md lg:text-xl mb-8 text-bolt-elements-textSecondary animate-fade-in animation-delay-200">
+                <p className="text-md lg:text-xl mb-4 text-bolt-elements-textSecondary animate-fade-in animation-delay-200">
                   Bring ideas to life in seconds or get help on existing projects.
                 </p>
               </div>
@@ -340,7 +340,6 @@ export const BaseChat = React.forwardRef<HTMLDivElement, BaseChatProps>(
               className={classNames('pt-6 px-2 sm:px-6', {
                 'h-full flex flex-col': chatStarted,
               })}
-              ref={scrollRef}
             >
               <ClientOnly>
                 {() => {
@@ -395,12 +394,6 @@ export const BaseChat = React.forwardRef<HTMLDivElement, BaseChatProps>(
                 <div
                   className={classNames(
                     'bg-bolt-elements-background-depth-2 p-3 rounded-lg border border-bolt-elements-borderColor relative w-full max-w-chat mx-auto z-prompt',
-
-                    /*
-                     * {
-                     *   'sticky bottom-2': chatStarted,
-                     * },
-                     */
                   )}
                 >
                   <svg className={classNames(styles.PromptEffectContainer)}>
@@ -627,7 +620,7 @@ export const BaseChat = React.forwardRef<HTMLDivElement, BaseChatProps>(
                 </div>
               </div>
             </div>
-            <div className="flex flex-col justify-center gap-5">
+            <div className="flex flex-col justify-center gap-2 mt-2">
               {!chatStarted && (
                 <div className="flex justify-center gap-2">
                   {ImportButtons(importChat)}
